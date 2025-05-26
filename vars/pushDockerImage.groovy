1df5c43c6378a14f4d8c3ac8a079cec91f392eb8
Aya Ahmed <89293797+AyaAkai@users.noreply.github.com>
2025-05-26 18:34:22 +0300

    Update pushDockerImage.groovy

--- vars/pushDockerImage.groovy
+++ vars/pushDockerImage.groovy
@@ -1,7 +1,7 @@
 def call(String imageName) {
-    echo "Pushing Docker image to DockerHub"
-    withCredentials([usernamePassword(credentialsId: 'dockerhub-cred', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
-        sh "echo \$DOCKER_PASSWORD | docker login -u \$DOCKER_USERNAME --password-stdin"
-        sh "docker push ${imageName}:v${env.BUILD_NUMBER}"
+    withCredentials([string(credentialsId: 'DOCKER_PASSWORD', variable: 'DOCKER_PASSWORD')]) {
+        sh "echo \$DOCKER_PASSWORD | docker login -u ayaahmed123 --password-stdin"
+        sh "docker push ${imageName}"
     }
 }
+
